09d53c1de428ba9a59eed6ed0fb871aca7d766b0
wh <15996589@qq.com>
2024-11-18 18:10:57 +0800

    20-虚拟dom渲染实现

--- packages/runtime-dom/src/index.ts
+++ packages/runtime-dom/src/index.ts
@@ -1,8 +1,12 @@
-export * from '@vue1/reactivity'
-
 import { nodeOps } from './nodeOps'
 import patchProp from './patchProp'
 
+import { createRenderer } from '@vue1/runtime-core'
+
 export const renderOptions = Object.assign({ patchProp }, nodeOps)
 
-export function createRenderer() {}
+export const render = (vnode, container) => {
+  // render方法采用dom api来进行渲染
+  return createRenderer(renderOptions).render(vnode, container)
+}
+export * from '@vue1/runtime-core'
